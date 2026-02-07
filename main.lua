bcee48d8ce91918d4868295f7d30476b113f5fd3
ollie <35317240+ollieiswoke@users.noreply.github.com>
2026-02-07 21:58:35 +1100

    uhhh ummm

--- main.lua
+++ main.lua
@@ -1,17 +1,32 @@
---[[pod_format="raw",created="2024-03-24 00:48:06",modified="2026-02-07 03:42:31",revision=125]]
+--[[pod_format="raw",created="2024-03-24 00:48:06",modified="2026-02-07 07:36:10",revision=151]]
 -- testing
 include "movement.lua"
+include "math.lua"
 function _init()
 	poke(0x5f5c, 255) -- diasable key repeat
 	p = {
 		x = 16*4,
 		y = 16*4,
-		sx = 0,
-		sy = 0,
-		smax = 3
+		vx = 0,
+		vy = 0,
+		smax = 3,
+		width = 32,
+		height = 32,
+		x_offset = 4,
+		y_offset = 4
 	}
+	-- box
+	b = {
+		x = 16*8,
+		y = 16*8,
+		width = 32,
+		height = 32
+	}
+	-- collision blocks
+	c = {4,}
 	-- acceleration
 	a = 0.3
+	
 end
 
 function _update()
@@ -26,5 +41,11 @@ function _draw()
 	cls()
 	map()
 	spr(9,p.x, p.y)
-	
+	print(math.floor(p.x), p.x, p.y-16)
+	print(math.floor(p.y), p.x+16, p.y-16)
+
+	spr(12,b.x, b.y)
+	print(math.floor(b.x), b.x, b.y-16)
+	print(math.floor(b.y), b.x+16, b.y-16)
+
 end
--- movement.lua
+++ movement.lua
@@ -1,6 +1,8 @@
---[[pod_format="raw",created="2026-02-07 02:41:15",modified="2026-02-07 02:41:15",revision=0]]
+--[[pod_format="raw",created="2026-02-07 02:41:15",modified="2026-02-07 10:57:27",revision=394]]
+include "math.lua"
+-- Handles Movement and Collision
 function move_player()
-    START_SPEED = 1
+   START_SPEED = 1
 	-- left
 	if (
 			btnp(0) and 
@@ -9,9 +11,9 @@ function move_player()
 			not btn(3) 
 		)
 		then
-		p.sx = -START_SPEED 
+		p.vx = -START_SPEED 
 	elseif btn(0) then
-		p.sx -= a
+		p.vx -= a
 	end
 	-- right
 	if (
@@ -21,9 +23,9 @@ function move_player()
 			not btn(3) 
 		)
 		then
-		p.sx = START_SPEED 
+		p.vx = START_SPEED 
 	elseif btn(1) then
-		p.sx += a
+		p.vx += a
 	end
 	-- up (if up and no other keys)
 	if (
@@ -33,9 +35,9 @@ function move_player()
 			not btn(3) 
 		)
 		then
-		p.sy = -START_SPEED 
+		p.vy = -START_SPEED 
 	elseif btn(2) then
-		p.sy -= a
+		p.vy -= a
 	end
 	-- down
 	if (
@@ -45,29 +47,89 @@ function move_player()
 			not btn(2) 
 		)
 		then
-		p.sy = START_SPEED 
+		p.vy = START_SPEED 
 	elseif btn(3) then
-		p.sy += a
+		p.vy += a
 	end
 
-
-	p.sx *= 0.90
-	p.sy *= 0.90
-	-- if not moving left or right, deccel x axis
---	if not (
---		btn(0) or
---		btn(1) or
---		btn(2) or
---		btn(3)
---		)
---	 then
---	 -- 0.95 for that luigi wavedash feel
---		p.sx *= 0.85
---		p.sy *= 0.85
---	end
+	-- 0.95 for that luigi wavedash feel
+	p.vx *= 0.90
+	p.vy *= 0.90
 	
-	
-	p.x += mid(-p.smax, p.sx, p.smax)
-	p.y += mid(-p.smax, p.sy, p.smax)
+	proposed_x = p.x + mid(-p.smax, p.vx, p.smax)
+	proposed_y = p.y + mid(-p.smax, p.vy, p.smax)
+	-- get proposed borders around the character
+	-- used for collision against walls
+	-- assumes top left pixel of sprite
+	p_right	=	proposed_x - p.x_offset + p.width 
+	p_left 	= 	proposed_x + p.x_offset
+	p_up 		=  proposed_y + p.y_offset
+	p_down 	= 	proposed_y - p.y_offset + p.height
 
+	-- note: currently only supports one box (grimace emoji)
+	b_up_border 	= b.y
+	b_down_border 	= b.y + b.height
+	b_right_border 	= b.x + b.width
+	b_left_border 	= b.x
+	
+	BOX_PUSH_SPEED = 1
+
+	-- if top of player edge is lower than top of box edge
+
+	-- RIGHT
+	if proposed_x > p.x then
+		-- if right edge is NOT hitting collision map block, move		
+		if mget((p_right)/16, (p.y+p.y_offset)/16) != 4 then
+			-- box collision
+			if p_right >= b_left_border and -- if going right 'into' the box
+				p.x <= b_left_border and  	 -- but currently to the left of the box
+				p.y + p.height >= b.y and 	 -- bottom of player lower than top of box
+				p.y <= (b.y + b.height) then -- top edge of player above bottom edge of box
+				
+				b.x = b.x + min(mid(-p.smax, p.vx, p.smax), BOX_PUSH_SPEED)
+				p.x = b.x - b.width + p.x_offset 
+			else
+				p.x = proposed_x
+			end
+		else
+			p.x = ((math.floor(proposed_x/16))*16)+p.x_offset 
+		end
+	end
+	-- LEFT
+	if proposed_x < p.x then
+		if mget(p_left/16, (p.y+p.y_offset)/16) != 4 then
+			-- box collision
+
+			if proposed_x <= b_right_border and -- if proposed left is going to 'go into' the box
+				p.x >= b_right_border and -- but currently TO THE RIGHT of the box
+				p.y + p.height >= b.y and 	 -- bottom of player lower than top of box
+				p.y <= (b.y + b.height) then -- top edge of player above bottom edge of box
+--				 
+				b.x = b.x + max(mid(-p.smax, p.vx, p.smax), - BOX_PUSH_SPEED)
+				p.x = b.x + b.width
+			else
+				p.x = proposed_x
+			end
+		else
+			p.x = ((math.ceil((proposed_x)/16))*16) - p.x_offset
+		end
+	end
+	-- UP
+	if proposed_y < p.y then
+		if mget((p.x+p.x_offset)/16, (p_up)/16) != 4 then
+			p.y = proposed_y
+		else
+			p.y = (math.ceil(proposed_y/16))*16 - p.y_offset
+		end
+	end
+	if proposed_y > p.y then
+		if mget((p.x+p.x_offset)/16, (p_down)/16) != 4 then
+			p.y = proposed_y
+		else
+			p.y = ((math.floor(proposed_y/16))*16)+p.y_offset
+		end
+	end
+	
+	-- BOX PUSHING...
+	
 end
